caaa567907c5005ab815148fe1aa517980da9082
OpKris <kristofferopdahl@mail.com>
2020-10-14 11:44:08 +0200

    new enc file

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ terraform {
   backend "gcs" {
     bucket = "opkristerraformdemo"
     prefix = "terraformstate"
-    credentials = "terraform.json.enc"
+    credentials = "terraform.json"
   }
 }
 provider "google-beta" {
-  credentials = file("terraform.json.enc")
+  credentials = file("terraform.json")
   project     = "terraform-demo-opkris"
   region      = "us-central1"
   zone        = "us-central1-c"
